f5d76faffd465a545b942e00030d5f3fff8aaace
reze101 <reze101@naver.com>
2022-03-24 16:57:40 +0900

    first cmmit

---
+++ script.js
@@ -0,0 +1,44 @@
+// function greet(name){
+//   console.log("안녕 내 이름은",name,"야")
+//   return name
+// }
+// let name = greet("야옹이")
+// console.log(name)
+
+
+// function meetAt(year, month, day){
+//   let nowYear = year
+//   let nowMonth = month
+//   let nowDay = day
+
+//   if(nowDay){
+//     return console.log(nowYear+'/'+nowMonth+'/'+nowDay)
+//   }
+//     if(nowMonth){
+//     return console.log(nowYear+'/'+nowMonth)
+//   }
+//       if(nowYear){
+//     return console.log(nowYear)
+//   }
+// }
+// meetAt(1995, 10)
+
+// function findSmallestElement(arr){
+//   let arrFirstIdx = arr[0]
+//   if(arr.length ===0){
+//     return 0
+//   } 
+//   for (let index = 0; index < arr.length; index++) {
+
+//     if(arrFirstIdx > arr[index]){
+//       arrFirstIdx =  arr[index]
+//     }
+    
+//   }  
+  
+//   return arrFirstIdx;
+// }
+
+// let num = findSmallestElement([])
+// console.log(num)
+
